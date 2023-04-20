41a68e6ed42fcadd00ea9bcce657bba8d44b1720
wilitp <guilledeipola@gmail.com>
2023-04-20 00:59:47 -0300

    Extraer funcion getColorInfo para usar en ambos generadores de ordenes

--- APIParte2.c
+++ APIParte2.c
@@ -1,18 +1,21 @@
+#include <assert.h>
 #include <stdlib.h>
 
-#include "APIParte2.h"
 #include "APIG23.h"
-
+#include "APIParte2.h"
 
 u32 Greedy(Grafo G, u32 *Orden, u32 *Color) { return 0; }
 
 char OrdenImparPar(u32 n, u32 *Orden, u32 *Color) { return '0'; }
 
 // F definida en la spec para el orden jedi
+// Tuve que hacerla variable global porq qsort_r no esta permitida en c99 (y los
+// profes compilan con ese standard)
 static u32 *F;
 
 // Comparador para ordenar V_i's en el orden Jedi
-// Asume que cuando se la llama F ya esta inicializada ya punta a un lugar de memoria valido
+// Asume que cuando se la llama F ya esta inicializada ya punta a un lugar de
+// memoria valido
 int compar(const void *v1, const void *v2) {
   u32 vfirst = *((u32 *)v1);
   u32 vsecond = *((u32 *)v2);
@@ -24,9 +27,8 @@ int compar(const void *v1, const void *v2) {
     return 1;
 }
 
-// ordena indices en la forma especial dada en las especificaciones
-char OrdenJedi(Grafo G, u32 *Orden, u32 *Color) { 
-  // Esta implementacion evita ordenar n vertices. Pero capaz era mas facil y usabamos mucha menos memoria xd
+u32 contarColores(Grafo G, u32 *Color) {
+
   u32 colorc = 0;
 
   // Buscamos el color maximo (r-1 en la especificacion)
@@ -36,31 +38,40 @@ char OrdenJedi(Grafo G, u32 *Orden, u32 *Color) {
   }
   // Corrijo para que sea la cantidad de colores en vez del maximo color
   ++colorc;
+  return colorc;
+}
 
+struct ColorInfo {
+  u32 colorc;
+  u32 **V;
+  u32 *cantidades;
+};
+
+// Devuelve los V_i's, sus cardinales y la cantidad de colores
+// el usuario de la funcion es el duenio de la referencia devuelta
+struct ColorInfo *getColorInfo(Grafo G, u32 *Color) {
+
+  struct ColorInfo *colorInfo = malloc(sizeof(struct ColorInfo));
+
+  colorInfo->colorc = contarColores(G, Color);
   // Los V_i del teorico
-  u32 **V = malloc((colorc) * sizeof(u32));
+  u32 **V = malloc(colorInfo->colorc * sizeof(u32));
 
   // Cantidad de vertices por color
   // cantidades Color -> Nat
-  u32 *cantidades = malloc((colorc) * sizeof(u32));
-  u32 *contadores = malloc((colorc) * sizeof(u32));
-  // Orden de los colores segun F
-  u32 *ordenColores = malloc((colorc) * sizeof(u32));
-
-  // Funcion F de la spec
-  F = malloc((colorc) * sizeof(u32));
+  colorInfo->cantidades = malloc(colorInfo->colorc * sizeof(u32));
 
   // Computamos F y las cantidades
   for (u32 i = 0; i < NumeroDeVertices(G); ++i) {
-    F[Color[i]] += Grado(i, G);
-    ++cantidades[Color[i]];
+    ++(colorInfo->cantidades[Color[i]]);
   }
 
   // hago espacio para cada V_i e inicializo ordenColores
-  for (u32 i = 0; i < colorc; ++i) {
-    V[i] = malloc(cantidades[i] * (sizeof(u32)));
-    ordenColores[i] = i;
+  for (u32 i = 0; i < colorInfo->colorc; ++i) {
+    V[i] = malloc(colorInfo->cantidades[i] * (sizeof(u32)));
   }
+
+  u32 *contadores = malloc((colorInfo->colorc) * sizeof(u32));
 
   // agrego los vertices a sus V_i
   for (u32 i = 0; i < NumeroDeVertices(G); ++i) {
@@ -68,20 +79,59 @@ char OrdenJedi(Grafo G, u32 *Orden, u32 *Color) {
     ++contadores[Color[i]];
   }
   // al final de esto debe cumplirse contadores = cantidades
-  
+
+  return colorInfo;
+}
+
+// ordena indices en la forma especial dada en las especificaciones
+char OrdenJedi(Grafo G, u32 *Orden, u32 *Color) {
+
+  assert(F == NULL);
+  // Conseguimos los V_i's, sus cardinales y la cantidad de colores
+  struct ColorInfo *colorInfo = getColorInfo(G, Color);
+
+  // Orden de los colores segun F
+  u32 *ordenColores = malloc((colorInfo->colorc) * sizeof(u32));
+
+  // Funcion F de la spec
+  F = calloc(colorInfo->colorc, sizeof(u32));
+
+  // Computamos F
+  for (u32 i = 0; i < NumeroDeVertices(G); ++i) {
+    F[Color[i]] += Grado(i, G);
+  }
+
+  // inicializo ordenColores
+  for (u32 i = 0; i < colorInfo->colorc; ++i) {
+    ordenColores[i] = i;
+  }
+
   // Ordeno los V_i's segun F
-  qsort(ordenColores, colorc, sizeof(u32), compar);
+  qsort(ordenColores, colorInfo->colorc, sizeof(u32), compar);
 
   // Usando ordenColores y cantidades, aplano V para obtener el Orden Jedi
-  for (u32 i = 0, j = 0; i < NumeroDeVertices(G) && j < colorc; ++i, ++j) {
-    while (contadores[j] > 0) {
-      Orden[i] = V[j][i];
-      --contadores[j];
+  for (u32 j = 0, i = 0, k = 0;
+       k < NumeroDeVertices(G) && j < colorInfo->colorc; ++k) {
+
+    Orden[k] = colorInfo->V[ordenColores[j]][i];
+    ++i;
+
+    // Si ya terminamos con este color pasamos al siguiente
+    if (i >= colorInfo->cantidades[j]) {
+      i = 0;
+      ++j;
     }
   }
-  // al final de esto contadores[i] = 0 forall i
 
-  // TODO: free de todo xd
+  // Free de todo
+  for (u32 i = 0; i < colorInfo->colorc; ++i) {
+    free(colorInfo->V[i]);
+  }
+  free(colorInfo->cantidades);
+  free(colorInfo->V);
+  free(colorInfo->V);
+  free(F);
+  F = NULL; // Dejo F como NULL para proximas iteraciones
 
-  return '0'; 
+  return '0';
 }
